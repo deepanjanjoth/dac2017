c51b5740f6f649d904c8b2f5d473881706f2bff1
santosh <santosh.ece06@gmail.com>
2017-04-17 14:32:14 +0530

    commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// alert("HELLO DAC!!!");
+
+var i = 0;
+var j = 0.0;
+var str = "Hello CDAC";
+
+
+
+function firstMethod(){
+   alert(str);
+
+   var temp = document.getElementById("home");
+   temp.innerHTML = "Modified Home";
+
+   var userName = document.getElementById("userNameId");
+   userName.value = "AAAA";
+
+
+};
