e5d7a16a3f37aa8a82f2f0bc276c85b414fe12a9
santosh <santosh.ece06@gmail.com>
2017-04-20 22:50:59 +0530

    commit

--- day6-1/main.js
+++ day6-1/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by santosh on 4/20/17.
  */
-
+var imageList = ["img/nodejs-vs-java.png", "img/141.png", "img/cool.jpg", "img/infinite_truth.jpg"];
 
 function increaseCount(elemObj){
     var currentCount = elemObj.children[1].innerHTML;
@@ -17,6 +17,8 @@ function postImage() {
     var newImgObj = refImgObj.cloneNode(true);
     newImgObj.style.display = "block";
     newImgObj.removeAttribute("id");
+
+    newImgObj.children[0].children[0].src = imageList[Math.floor(Math.random() * 4)];
 
 
     var parentObj = document.getElementById("parentId");
