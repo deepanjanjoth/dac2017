5d9befecf1658fee2ede41b99b53678513b1fe46
santosh <santosh.ece06@gmail.com>
2017-05-06 12:20:25 +0530

    commit

---
+++ day10/client.js
@@ -0,0 +1,46 @@
+/**
+ * Created by santosh on 5/6/17.
+ */
+
+function ajaxTest() {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "./server.json", true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+
+           var resObj =  JSON.parse(xhttp.responseText);
+           console.log(resObj);
+
+           postContentUsingQueryAjaxOnLoad(resObj);
+        }
+
+    }
+}
+
+
+
+function postContentUsingQueryAjaxOnLoad(resObj) {
+    var newObj = $("#refIdForContent").clone(true).css("display", "block").removeAttr("id");
+
+    var dynamicContent = resObj.quote;
+    newObj.children().children().first().html(dynamicContent);
+
+    $("#parentId").prepend(newObj);
+
+    // some logic to display notification
+    $("#notificationId").fadeIn(2000, function(){
+        $("#notificationId").fadeOut(5000);
+    });
+}
+
+
+
+
+// initiate ajax call;
+ajaxTest();
